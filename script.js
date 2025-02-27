7f49ed0cbf0f4ddb9a6e3e5a3e2b610a0ea81fd0
NITISH KUMAR KADIVETI <125491723+Nitish-Kadiveti@users.noreply.github.com>
2025-02-27 19:57:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Engineer", "Designer"],
+        strings: ["Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
