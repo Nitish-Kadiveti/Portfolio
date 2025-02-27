545aad4272c15dff1c30fdabb525421c20425206
NITISH KUMAR KADIVETI <125491723+Nitish-Kadiveti@users.noreply.github.com>
2025-02-27 19:53:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Blogger", "Designer"],
+        strings: ["Developer", "Engineer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Blogger", "Designer"],
+        strings: ["Developer", "Engineer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
